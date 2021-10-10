59f7cc5701d284ecc30dc0720718ee40488c0bd1
Keisuke Nagashima <energyrevo21@gmail.com>
2021-10-10 20:45:14 +0900

    Remove unnecessary spring-security Spring security requires signin by default but it's annoying in development

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,7 +25,6 @@ repositories {
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.flywaydb:flyway-core")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
@@ -33,7 +32,6 @@ dependencies {
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("org.springframework.security:spring-security-test")
 }
 
 tasks.withType<KotlinCompile> {
