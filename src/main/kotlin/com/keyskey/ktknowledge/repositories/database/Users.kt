2c8f5eb1fc8a2b601d9b495c33d9d7aec6f0693e
Keisuke Nagashima <energyrevo21@gmail.com>
2021-11-01 00:47:09 +0900

    Move database package under repository and define record class

--- src/main/kotlin/com/keyskey/ktknowledge/repositories/database/Users.kt
+++ src/main/kotlin/com/keyskey/ktknowledge/repositories/database/Users.kt
@@ -1,9 +1,10 @@
-package com.keyskey.ktknowledge.database
+package com.keyskey.ktknowledge.repositories.database
 
 import org.ktorm.schema.Table
 import org.ktorm.schema.datetime
 import org.ktorm.schema.int
 import org.ktorm.schema.varchar
+import java.time.LocalDateTime
 
 object Users: Table<Nothing>("users") {
     val id = int("id").primaryKey()
@@ -11,3 +12,5 @@ object Users: Table<Nothing>("users") {
     val createdAt = datetime("created_at")
     val updatedAt = datetime("updated_at")
 }
+
+data class UserRecord(val id: Int?, val name: String?, val createdAt: LocalDateTime?, val updatedAt: LocalDateTime?)
