7a70192f58c84e3545df357de85c938ba273c31d
Keisuke Nagashima <energyrevo21@gmail.com>
2021-11-13 20:17:57 +0900

    Add created and updatedAt to UserType

--- src/main/kotlin/com/keyskey/ktknowledge/handlers/graphql/types/UserType.kt
+++ src/main/kotlin/com/keyskey/ktknowledge/handlers/graphql/types/UserType.kt
@@ -2,10 +2,13 @@ package com.keyskey.ktknowledge.handlers.graphql.types
 
 import com.expediagroup.graphql.generator.scalars.ID
 import com.keyskey.ktknowledge.entities.User
+import java.time.LocalDateTime
 
-data class UserType (val id: ID, val name: String)
+data class UserType (val id: ID, val name: String, val createdAt: LocalDateTime, val updatedAt: LocalDateTime)
 
 fun User.toUserType(): UserType = UserType(
     id = ID(id.toString()),
-    name = name
+    name = name,
+    createdAt = createdAt,
+    updatedAt = updatedAt
 )
