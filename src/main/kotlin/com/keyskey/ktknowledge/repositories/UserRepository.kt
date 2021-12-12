2bb1d10bd5653d1c766fae9da8a4ebe33ec178a1
Keisuke Nagashima <energyrevo21@gmail.com>
2021-12-12 18:26:58 +0900

    Let UserRepository to handle user email and password

--- src/main/kotlin/com/keyskey/ktknowledge/repositories/UserRepository.kt
+++ src/main/kotlin/com/keyskey/ktknowledge/repositories/UserRepository.kt
@@ -11,15 +11,26 @@ class UserRepository(val database: Database) {
     private fun QueryRowSet.toUser(): User {
         val id = this[Users.id]
         val name = this[Users.name]
+        val email = this[Users.email]
+        val password = this[Users.password]
         val createdAt = this[Users.createdAt]
         val updatedAt = this[Users.updatedAt]
 
         checkNotNull(id)
         checkNotNull(name)
+        checkNotNull(email)
+        checkNotNull(password)
         checkNotNull(createdAt)
         checkNotNull(updatedAt)
 
-        return User(id, name, createdAt, updatedAt)
+        return User(
+            id = id,
+            name = name,
+            email = email,
+            password = password,
+            createdAt = createdAt,
+            updatedAt = updatedAt
+        )
     }
 
     fun findAll(): List<User> {
@@ -42,6 +53,8 @@ class UserRepository(val database: Database) {
     fun create(user: User): User {
         val id = database.insertAndGenerateKey(Users) {
             set(it.name, user.name)
+            set(it.email, user.email)
+            set(it.password, user.password)
             set(it.createdAt, user.createdAt)
             set(it.updatedAt, user.updatedAt)
         }.toString().toInt()
