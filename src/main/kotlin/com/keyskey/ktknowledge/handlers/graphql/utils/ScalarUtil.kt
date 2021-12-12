254cb732c2aaf2b1fec34727410a6c24eaa771a0
Keisuke Nagashima <energyrevo21@gmail.com>
2021-12-12 21:58:35 +0900

    Add `User:` prefix to the ID field of UserType to follow Relay spec

--- src/main/kotlin/com/keyskey/ktknowledge/handlers/graphql/utils/ScalarUtil.kt
+++ src/main/kotlin/com/keyskey/ktknowledge/handlers/graphql/utils/ScalarUtil.kt
@@ -1,7 +1,18 @@
 package com.keyskey.ktknowledge.handlers.graphql.utils
 
 import com.expediagroup.graphql.generator.scalars.ID
+import graphql.relay.Relay
 
-fun ID.toInt(): Int = this.toString().toInt()
+fun ID.toInt(): Int {
+    // IDは `User:1` のような値がBase64エンコーディングされたものであることを期待している
+    // Relay#fromGlobalId はこのIDをデコードした上で`:`でsplitし、type: "User と id: "1" を持つResolvedGlobalIdのインスタンスを返してくれる
+    val globalId: Relay.ResolvedGlobalId = Relay().fromGlobalId(this.toString())
 
-fun Int.toID(): ID = ID(this.toString())
+    return globalId.id.trim().toInt()
+}
+
+fun Int.toID(type: String): ID {
+    val globalId: String = Relay().toGlobalId(type, this.toString())
+
+    return ID(globalId)
+}
