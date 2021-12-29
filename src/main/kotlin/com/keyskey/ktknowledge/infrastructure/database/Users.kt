8407cec264265d6687d6c98a1d1c019f9d5b396a
Keisuke Nagashima <energyrevo21@gmail.com>
2021-12-30 04:03:29 +0900

    Implement UserRepository by Entity API Entity API doesn't provide a way to get generated auto increment key when inserting record. So it's impossible to reproduce the same logic as SQL DSL.

--- src/main/kotlin/com/keyskey/ktknowledge/infrastructure/database/Users.kt
+++ src/main/kotlin/com/keyskey/ktknowledge/infrastructure/database/Users.kt
@@ -1,15 +1,16 @@
 package com.keyskey.ktknowledge.infrastructure.database
 
+import com.keyskey.ktknowledge.infrastructure.repositories.UserDao
 import org.ktorm.schema.Table
 import org.ktorm.schema.datetime
 import org.ktorm.schema.int
 import org.ktorm.schema.varchar
 
-object Users: Table<Nothing>("users") {
-    val id = int("id").primaryKey()
-    val name = varchar("name")
-    val email = varchar("email")
-    val password = varchar("password")
-    val createdAt = datetime("created_at")
-    val updatedAt = datetime("updated_at")
+object Users: Table<UserDao>("users") {
+    val id = int("id").primaryKey().bindTo { it.id }
+    val name = varchar("name").bindTo { it.name }
+    val email = varchar("email").bindTo { it.email }
+    val password = varchar("password").bindTo { it.password }
+    val createdAt = datetime("created_at").bindTo { it.createdAt }
+    val updatedAt = datetime("updated_at").bindTo { it.updatedAt }
 }
